3bed0950c9332f2a2ed37b7985c19ebb00f2c4bf
Justen Quirante <jcquirante@gmail.com>
2019-03-17 00:29:34 -0700

    Completed adding accounts to Db

--- main.js
+++ main.js
@@ -14,3 +14,18 @@ function submitSignUpInfo(){
     }
     $.ajax(options);
 }
+
+function submitSignUpInfo2(){
+    console.log('hello');
+    event.preventDefault();
+    const options = {
+        url: "http://localhost:7777/SignUp2",
+        dataType: 'json',
+        method: 'GET',
+        data: $('form').serialize(),
+        headers: {
+            "Access-Control-Allow-Origin" : "*",
+        },
+    }
+    $.ajax(options);
+}
